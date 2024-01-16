aa322661bca51fe132cef4b1375358ccb0e7d9a5
Abdallah Magdy <abdullah.mahmoud02@eng-st.cu.edu.eg>
2024-01-16 18:25:18 +0200

    Code refactoring

--- view/Icon.py
+++ view/Icon.py
@@ -1,6 +1,9 @@
-from PyQt6 import QtWidgets , QtGui , QtCore
+from PyQt6 import QtGui
+
 
 class Icon(QtGui.QIcon):
-    def __init__(self, icon_path : str):
+    def __init__(self, icon_path: str):
         super().__init__()
-        self.addPixmap(QtGui.QPixmap(icon_path), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
+        self.addPixmap(
+            QtGui.QPixmap(icon_path), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off
+        )
